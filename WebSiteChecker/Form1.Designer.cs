6cb7521d9ef226bba368421851e385a4501a4914
Larry Liu <xiaozi7912@gmail.com>
2014-06-09 00:16:05 +0800

    init
    新增、刪除 網站
    自動檢查網站狀態
    
    Signed-off-by: Larry Liu <xiaozi7912@gmail.com>

---
+++ WebSiteChecker/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WebSiteChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 239);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(384, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(128, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(297, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "新增網站";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(8, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(283, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.LargeChange = 1;
+            this.hScrollBar1.Location = new System.Drawing.Point(9, 33);
+            this.hScrollBar1.Maximum = 5;
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(363, 20);
+            this.hScrollBar1.TabIndex = 3;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 56);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(360, 180);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseClick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "123";
+            this.notifyIcon1.BalloonTipTitle = "WebSite Checker";
+            this.notifyIcon1.Text = "WebSite Checker";
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WebSite Checker";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ WebSiteChecker/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebSiteChecker
+{
+    public partial class Form1 : Form
+    {
+        private BackgroundWorker bw = new BackgroundWorker();
+        private HttpWebRequest myRequest;
+        private string currDir = Directory.GetCurrentDirectory();
+        private string fileName = "website.txt";
+        private int updateTime = 1000 * 60;
+
+        public Form1()
+        {
+            InitializeComponent();
+            bw.DoWork += new DoWorkEventHandler(checkWebSite);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.Columns.Add("Status");
+            listView1.Columns.Add("Site");
+            listView1.Columns.Add("LastCheckTime");
+            listView1.View = View.Details;
+            listView1.FullRowSelect = true;
+            this.Icon = new Icon("icon001.ico");
+            notifyIcon1.Icon = new Icon("icon001.ico");
+
+            StreamReader sr = new StreamReader(string.Format("{0}/{1}", currDir, fileName));
+            while (!sr.EndOfStream)
+            {
+                string _line = sr.ReadLine();
+                listView1.Items.Insert(0, new ListViewItem(new string[] { _line.Split(new char[] { ',' })[0], _line.Split(new char[] { ',' })[1], _line.Split(new char[] { ',' })[2] }));
+                listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            }
+            sr.Close();
+
+            updateTimer();
+
+            if (!bw.IsBusy)
+            {
+                bw.RunWorkerAsync(new object[] { listView1 });
+            }
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            updateTimer();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Regex _regex = new Regex("http(|s)://.*");
+            string _text = textBox1.Text;
+
+
+            if (_text != string.Empty)
+            {
+                if (_regex.IsMatch(_text))
+                {
+                    bool itemExists = false;
+
+                    foreach (ListViewItem item in listView1.Items)
+                    {
+                        if (_text == item.SubItems[1].Text)
+                        {
+                            itemExists = true;
+                            break;
+                        }
+                    }
+
+                    if (!itemExists)
+                    {
+
+                        listView1.Items.Insert(0, new ListViewItem(new string[] { "Checking...", _text, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") }));
+                        listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+
+                        textBox1.Text = string.Empty;
+
+                        try
+                        {
+                            if (bw.IsBusy != true)
+                            {
+                                object[] args = { listView1, 0 };
+                                bw.RunWorkerAsync(args);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                    else
+                    {
+                        textBox1.Text = "Url 重複輸入！";
+                        textBox1.Focus();
+                        textBox1.SelectAll();
+                    }
+                }
+                else
+                {
+                    textBox1.Text = "Url 格式輸入錯誤！";
+                    textBox1.Focus();
+                    textBox1.SelectAll();
+                }
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == 13)
+            {
+                button1.PerformClick();
+            }
+        }
+
+        private void checkWebSite(object sender, DoWorkEventArgs e)
+        {
+            Invoke(new MethodInvoker(delegate
+            {
+                try
+                {
+                    Console.WriteLine("Running...");
+                    object[] objs = (object[])e.Argument;
+                    ListView _ls = (ListView)objs[0];
+
+                    foreach (ListViewItem item in _ls.Items)
+                    {
+                        Console.WriteLine(string.Format("WebSite : {0}", item.SubItems[1].Text));
+                        Console.WriteLine(string.Format("Status : {0}", item.SubItems[0].Text));
+                        item.SubItems[0].Text = "Checking...";
+                        listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+
+                        myRequest = (HttpWebRequest)HttpWebRequest.Create(item.SubItems[1].Text);
+                        myRequest.Method = WebRequestMethods.Http.Get;
+                        myRequest.BeginGetResponse(new AsyncCallback(finishWebRequest), myRequest);
+
+                        if (objs.Length == 2 && item.Index == 0)
+                        {
+                            break;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }));
+        }
+
+        void finishWebRequest(IAsyncResult result) {
+            Invoke(new MethodInvoker(delegate {
+                string _website = string.Empty;
+                try
+                {
+                    HttpWebRequest request = result.AsyncState as HttpWebRequest;
+                    _website = request.RequestUri.OriginalString;
+                    HttpWebResponse response = request.EndGetResponse(result) as HttpWebResponse;
+
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        foreach (ListViewItem item in listView1.Items)
+                        {
+                            if (item.SubItems[1].Text == response.ResponseUri.OriginalString)
+                            {
+                                item.SubItems[0].Text = response.StatusCode.ToString();
+                                item.SubItems[2].Text = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                                listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+                                break;
+                            }
+                        }
+                    }
+
+                    response.Close();
+
+                    Console.WriteLine(response.StatusCode);
+                }
+                catch (Exception ex)
+                {
+                    foreach (ListViewItem item in listView1.Items)
+                    {
+                        if (item.SubItems[1].Text.Equals(_website))
+                        {
+                            item.SubItems[0].Text = ex.Message;
+                            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+                            break;
+                        }
+                    }
+                }
+            }));
+        }
+
+        private void updateTimer()
+        {
+            timer1.Interval = (hScrollBar1.Value + 1) * 10 * updateTime;
+            timer1.Start();
+            toolStripStatusLabel1.Text = string.Format("更新時間：{0}分鐘", (hScrollBar1.Value + 1) * 10);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!bw.IsBusy)
+            {
+                bw.RunWorkerAsync(new object[] { listView1 });
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Console.WriteLine(e.Cancel);
+            Console.WriteLine(e.CloseReason.ToString());
+
+            StreamWriter sw = new StreamWriter(string.Format("{0}/{1}", currDir, fileName));
+
+            foreach (ListViewItem item in listView1.Items)
+            {
+                sw.WriteLine(string.Format("{0},{1},{2}", item.SubItems[0].Text, item.SubItems[1].Text, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")));
+            }
+
+            sw.Close();
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.Show();
+            WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+
+        private void listView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine(e.Button);
+            if (e.Button == MouseButtons.Right)
+            {
+                listView1.SelectedItems[0].Remove();
+            }
+        }
+    }
+}
